83c424531b80b3ba5c3b9fd343516a1b82e19704
lyndsiWilliams <kcatgirl@gmail.com>
2020-11-02 15:54:45 -0600

    Basic server created

--- url-shortener/src/App.js
+++ url-shortener/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+// Package imports
+import React from 'react';
+// Styling
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>myMinnow - Turn your big fish URL into a tiny minnow!</h1>
       </header>
     </div>
   );
